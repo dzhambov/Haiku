667161e8622e1de75b4d94511f124832595bad0e
Drake Wilcox and Hristo Dzhambov <drake@>
2020-02-05 11:09:03 -0800

    refactor haiku.js code to use classes instead of previous syntax.

--- src/haiku.js
+++ src/haiku.js
@@ -1,11 +1,13 @@
-export function Haiku (line1, line2, line3) {
-  this.line1 = line1;
-  this.line2 = line2;
-  this.line3 = line3;
-}
-Haiku.prototype.lineNumber = function() {
-  console.log(this);
-  var lineArray = Object.values(this);
-  console.log(lineArray);
-  return lineArray.length;
+export class Haiku {
+  constructor(line1, line2, line3) { 
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
+  lineNumber () {
+    console.log(this);
+    var lineArray = Object.values(this);
+    console.log(lineArray);
+    return lineArray.length;
+  }
 };

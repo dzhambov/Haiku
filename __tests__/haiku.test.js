2f217287509a552182dfc255de2ae285c85e070e
Drake Wilcox and Hristo Dzhambov <drake@>
2020-02-05 14:08:19 -0800

    test functions that count the amount of vowels in a sentence.

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,7 +2,7 @@ import { Haiku } from './../src/haiku.js';
 
 describe ('Haiku', () => {
   let haiku 
-    const line1 = "Like crunchy cornflakes"
+    const line1 = "Like crunchy popcorn"
     const line2 = "Gold leaves rustle underfoot";
     const line3 = "Beauty in decay";
   
@@ -12,5 +12,8 @@ describe ('Haiku', () => {
   test('should determine if the haiku contains 3 lines', () => {
     expect(haiku.lineNumber()).toEqual(3);
   });
+  test('should determine the amount of syllables in line a line', () =>{
+    expect(haiku.lineSyl()).toEqual(5);
+  });
 
-});
+});                            
--- src/haiku.js
+++ src/haiku.js
@@ -1,16 +1,25 @@
 export class Haiku {
   constructor(line1, line2, line3) { 
+    this.allLines = [line1, line2, line3];
     this.line1 = line1;
     this.line2 = line2;
     this.line3 = line3;
+    this.vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   }
   lineNumber () {
-    var lineArray = Object.values(this);
+    var lineArray = this.allLines;
     return lineArray.length;
   }
-  line1syl () {
-    let line1Words = this.line1.split(" ");
-    console.log(line1Words)
+  lineSyl () {
+    let line = this.line1.split("");
+    console.log(line);
+    let vowelCount = 0;
+    for (var i = 0; i < line.length; i++) {
+      if (this.vowels.includes(line[i])) {
+        vowelCount ++;
+      }
+      console.log(vowelCount);
+    }
+    return vowelCount;
   }
 };
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
